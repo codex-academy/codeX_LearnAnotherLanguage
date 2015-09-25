1c0f7ebb30936fd9269935e405b73d2f03ee4922
siphesande <siphesande@projectcodex.co>
2015-09-25 16:24:21 +0200

    guess_the_number

--- guess_the_number.py
+++ guess_the_number.py
@@ -11,7 +11,7 @@ print(myName + ", Wow! That's a Cool name")
 print("How many students we have here at CodeX?")
 print("Please guess a number between 20 and 30")
 while guessesTaken < 4:
-    print('Take a guess.') # There are four spaces in front of print.
+    print('Take a guess,Please!') # There are four spaces in front of print.
     guess = input()
     guess = int(guess)
 
@@ -30,6 +30,10 @@ if guess == number:
     guessesTaken = str(guessesTaken)
     print('Awesome!, ' + myName + '! You guessed CodeX students in ' + guessesTaken + ' guesses!')
 
-if guess != number:
-    number = str(number)
-    print('Nope/Uyaxoka(Xhosa). The number I was thinking of was ' + number)
+#if guess != number:
+elif guess != number:
+     number = str(number)
+     print('Uyaxoka(Xhosa). The number I was thinking of was ' + number)
+
+else:
+    print("You are idiot!")
