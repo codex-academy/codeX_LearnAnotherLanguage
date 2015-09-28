35d146c8c9a03f0b6336889a71b710ac9ec3211e
siphesande <siphesande@projectcodex.co>
2015-09-28 14:08:58 +0200

    madlib

---
+++ # this is.py
@@ -0,0 +1,11 @@
+# this is Mablib Program by S.MALGAS
+def madlib():
+    print "Welcome to my Mad Lib"
+	relative = raw_input("Enter a type of relative")
+	name = raw_input("Enter a name")
+
+	verb = raw_input("Enter an 'ing' verb")
+	ajective = raw_input("Enter an adjective ")
+	noun = raw_input("Enter a noun")
+
+   print = "My dear " , relative, name, "was", verb, "along the road when he/she was attacked by a ", adjective, noun
