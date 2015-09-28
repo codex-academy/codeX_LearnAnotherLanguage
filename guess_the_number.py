7cbdbd872e00490c1595b23ba7cd07f04e551a7a
siphesande <siphesande@projectcodex.co>
2015-09-28 09:48:50 +0200

    guess the number

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,21 +1,23 @@
 # This is a guess the number game about codeX student.
 import random
 
-guessesTaken = 0
 
-print('Hello! Please tell me your name! now!!')
+
+print('Welcome! Please tell me your name! now!!')
 myName = input()#This where you insert an input(use the Terminal)
+
+numberOFguesses = 0
 
 number = random.randint(20, 30)
 print(myName + ", Wow! That's a Cool name")
 print("How many students we have here at CodeX?")
 print("Please guess a number between 20 and 30")
-while guessesTaken < 4:
+while numberOFguesses < 4:
     print('Take a guess,Please!') # There are four spaces in front of print.
     guess = input()
     guess = int(guess)
 
-    guessesTaken = guessesTaken + 1
+    numberOFguesses = numberOFguesses + 1
 
     if guess < number:
         print(myName + ',Your guess is too low.') # There are eight spaces in front of print.
@@ -27,13 +29,13 @@ while guessesTaken < 4:
         break
 
 if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Awesome!, ' + myName + '! You guessed CodeX students in ' + guessesTaken + ' guesses!')
+   numberOFguesses  = str(numberOFguesses)
+   print('Awesome!, ' + myName + '! You guessed CodeX students in ' + numberOFguesses + ' guesses!')
 
 #if guess != number:
 elif guess != number:
      number = str(number)
-     print('Uyaxoka(Xhosa). The number I was thinking of was ' + number)
+     print('Wrong!. The number I was thinking of was ' + number)
 
 else:
     print("You are idiot!")
